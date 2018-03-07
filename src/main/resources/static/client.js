bd8391bae12c5119df4a3c75c72b577a05b71a2d
Ted <ted.stark@gmail.com>
2018-03-07 13:15:53 -0700

    Automatic roster generation added

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 
 function init() {
     enableButtons();
+    generateEmps()
 }
 function enableButtons(){
     $("#btnAdd").on("click",postEmployee);
@@ -31,7 +32,8 @@ function postEmployee() {
         url: "/employees",
         data: JSON.stringify(newEmployee),
         success: function(response){
-            console.log(response)
+            console.log(response);
+            showNew(response);
         }
     })
 }
@@ -45,8 +47,25 @@ function getRoster() {
         }
     });
 }
+function showNew(response) {
+    $("#tableNew").empty();
+    // for (var i = 0; i < response._embedded.employees.length; i++) {
+    //     var employee = response._embedded.employees[i];
+        var employee = response;
+        $("#tableNew").append("<tr></tr>");
+        var el = $("#tableNew").children().last();
+        el.append("<th scope=\"row\">"+ employee.fakeid+ "</th>\n" +
+            "      <td>"+ employee.lname+ "</td>\n" +
+            "      <td>"+ employee.fname+ "</td>\n" +
+            "      <td>"+ employee.salary+ "</td>\n" +
+            "      <td>"+employee.position+"</td>");
+        $("#addMessage").text("Employee Added!");
+    }
+
 function appendRoster(response) {
     $("#tableRoster").empty();
+    $("#tableNew").empty();
+    $("#addMessage").text("");
     for (var i = 0; i < response._embedded.employees.length; i++) {
         var employee = response._embedded.employees[i];
         $("#tableRoster").append("<tr></tr>");
@@ -56,5 +75,59 @@ function appendRoster(response) {
             "      <td>"+ employee.fname+ "</td>\n" +
             "      <td>"+ employee.salary+ "</td>\n" +
             "      <td>"+employee.position+"</td>");
+    }
+}
+function randName() {
+    var name = "";
+    var range = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (var i = 0; i < 6; i++)
+        name += range.charAt(Math.floor(Math.random() * range.length));
+    return name;
+}
+function randFname() {
+    var array = ['Jake', 'Koddie', 'Joan', 'Ted', 'Mark', 'Manny', 'Lori', 'Kym', 'Scott','Zech'];
+    var fname = array[Math.floor(Math.random() * array.length)];
+    return fname;
+}
+function randLname() {
+    var array = ['Taylor', 'Becker', 'Adams', 'Stark', 'Feinman', 'Aguilar', 'Tie-Shue', 'Hsu', 'Bromander','Heneveld'];
+    var lname = array[Math.floor(Math.random() * array.length)];
+    return lname;
+}
+function randPos() {
+    var array = ['Buyer', 'Cashier', 'Greeter', 'HR', 'Manager', 'Security'];
+    var position = array[Math.floor(Math.random() * array.length)];
+    return position;
+}
+function randSal() {
+    var rand=Math.floor((Math.random() * 45) + 45);
+    var salary=rand*1000;
+    return salary;
+}
+function generateEmps() {
+    for (var i = 0; i < 10; i++) {
+        var id = Math.floor((Math.random() * 499999) + 500000);
+        var fname = randFname();
+        var lname = randLname();
+        var position = randPos();
+        var salary = randSal();
+        var newEmployee = {
+            id: id,
+            fakeid: id,
+            fname: fname,
+            lname: lname,
+            position: position,
+            salary: salary
+        };
+        $.ajax({
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            url: "/employees",
+            data: JSON.stringify(newEmployee),
+            success: function (response) {
+                console.log(response)
+            }
+        });
     }
 }
